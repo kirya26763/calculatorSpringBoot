55d1045849813d66463736903debbca6fa358345
Arty <kirya26763@mail.ru>
2024-12-14 21:18:27 +0700

    init

--- src/main/java/Controller/CalculatorController.java
+++ src/main/java/Controller/CalculatorController.java
@@ -1,4 +1,4 @@
-package pro.sky.calculatorSprinBoot;
+package Controller;
 
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
