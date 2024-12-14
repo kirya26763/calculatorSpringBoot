31a49bb19ac3d586cd890a0158e5a5e48618caa9
Arty <kirya26763@mail.ru>
2024-12-14 17:24:30 +0700

    init

---
+++ src/main/java/pro/sky/calculatorSprinBoot/CalculatorController.java
@@ -0,0 +1,37 @@
+package pro.sky.calculatorSprinBoot;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController {
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping(path = "/calculator")
+    public String calculator() {
+        return calculatorService.calculator();
+    }
+    @GetMapping(path = "/plus")
+    public String plus (@RequestParam int num1, @RequestParam int num2){
+        return calculatorService.plus(num1,num2);
+    }
+    @GetMapping(path = "/minus")
+    public String minus (@RequestParam int num1,@RequestParam int num2){
+        return calculatorService.minus(num1,num2);
+    }
+    @GetMapping(path = "multiply")
+    public String multiply (@RequestParam int num1,@RequestParam int num2){
+        return calculatorService.multiply(num1,num2);
+    }
+    @GetMapping(path = "/divide")
+    public String divide (@RequestParam int num1,@RequestParam int num2){
+        return calculatorService.divide(num1,num2);
+    }
+}
